0e271f39a24b3fae15f5d2f98cab9f3646c427fa
na052 <na052@192.168.219.100>
2021-06-17 01:25:38 +0900

    UPDATEAEAETATTATA

---
+++ CHASpringBoard/src/main/java/com/SpringBoard/dto/UserDTO.java
@@ -0,0 +1,82 @@
+package com.SpringBoard.dto;
+/*
+ * 테이블명=>CHAUSER
+  이름         널?       유형            
+---------- -------- ------------- 
+ID         NOT NULL VARCHAR2(20)  -아이디
+PASSWD              VARCHAR2(200) -비밀번호
+NAME                VARCHAR2(20)  -이름
+EMAIL               VARCHAR2(70)  -이메일
+JOIN_DATE           DATE          -회원가입일
+LAST_LOGIN          DATE          -마지막 로그인
+STATUS              NUMBER(1)   -회원 상태 : 1(일반회원),9(관리자)
+ * */
+public class UserDTO {
+	private String id;
+	private String passwd;
+	private String name;
+	private String email;
+	private String joinDate;
+	private String lastLogin;
+	private int status;
+	
+	public UserDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPasswd() {
+		return passwd;
+	}
+
+	public void setPasswd(String passwd) {
+		this.passwd = passwd;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getJoinDate() {
+		return joinDate;
+	}
+
+	public void setJoinDate(String joinDate) {
+		this.joinDate = joinDate;
+	}
+
+	public String getLastLogin() {
+		return lastLogin;
+	}
+
+	public void setLastLogin(String lastLogin) {
+		this.lastLogin = lastLogin;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+
+	public void setStatus(int status) {
+		this.status = status;
+	}
+}
---
+++ CHASpringBoard/src/main/webapp/WEB-INF/views/join_User.jsp
@@ -0,0 +1,41 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>CHA SPRING BOARD</title>
+</head>
+<body>
+	<table>
+		<tr>
+			<td>아이디</td>
+			<td>
+				<input type="text" name="id" value="">
+			</td>
+		</tr>
+		<tr>
+			<td>비밀번호</td>
+			<td>
+				<input type="text" name="passwd" value="">
+			</td>
+		</tr>
+		<tr>
+			<td>이름</td>
+			<td>
+				<input type="text" name="name" value="">
+			</td>
+		</tr>
+		<tr>
+			<td>이메일</td>
+			<td>
+				<input type="text" name="email" value="">
+			</td>
+		</tr>
+		<tr>
+			<td></td>
+		</tr>
+	</table>
+	
+</body>
+</html>
